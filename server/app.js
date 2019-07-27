b7241c20d1307cd57a38455250e5994660830e46
John Sullivan <sulljohn12@gmail.com>
2019-07-26 22:21:21 -0400

    missing files

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/graphql');
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
+// when hit graphql route, handoff to graphqlHTTP
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
+});
